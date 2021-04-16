55e3c95045d0e078d8a152666e9b0d1fcbbf439d
Mike Davies <bsmd@leeds.ac.uk>
2021-04-16 16:05:59 +0100

    Include maven execute plugin for easy running

--- src/main/java/com/mkdevs/App.java
+++ src/main/java/com/mkdevs/App.java
@@ -1,13 +1,48 @@
 package com.mkdevs;
 
+import java.util.List;
+
+import com.mkdevs.FunctionCaller.FunctionOption;
+
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Hello world!
  *
  */
+@Slf4j
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	FunctionCaller caller = AppConstructor.init();
+
+    	DiceFactory factory = AppConstructor.diceFactory();
+    	AppConstructor.initialiseRepoWithDice(List.of(
+    			factory.createDice("D6", "D6"),
+    			factory.createDice("FixedD20", "D20-fixed"),
+    			factory.createDice("D20", "D20")
+    			));
+    	
+    	caller.addFunction(new FunctionOption() {
+			
+			@Override
+			public String id() {
+				return "Exit";
+			}
+			
+			@Override
+			public void doIt() {
+				System.exit(0);
+			}
+		});
+    	
+    	while(true) {
+    		try {
+    			caller.askAndCall();
+    		} catch (Exception e) {
+    			log.error("the program encountered a problem and has to close", e);
+			}
+    	}
     }
 }
